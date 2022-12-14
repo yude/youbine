57bfe9b2994a8e34c39e1776404bdda212298028
yude <i@yude.jp>
2022-12-14 23:12:03 +0000

    feat: retrieve DB credentials from environment value

--- app/database/utils.go
+++ app/database/utils.go
@@ -1,12 +1,24 @@
 package database
 
-import "github.com/go-pg/pg"
+import (
+	"os"
+
+	"github.com/go-pg/pg"
+)
 
 func GetCredentials() *pg.DB {
 	db := pg.Connect(&pg.Options{
-		User:     "app",
-		Password: "app",
-		Database: "app",
+		User:     GetEnv("POSTGRES_USER", "app"),
+		Password: GetEnv("POSTGRES_PASSWORD", "app"),
+		Database: GetEnv("POSTGRES_DB", "app"),
 	})
 	return db
+}
+
+func GetEnv(key, fallback string) string {
+	value, exists := os.LookupEnv(key)
+	if !exists {
+		value = fallback
+	}
+	return value
 }
