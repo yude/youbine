0378c137cd753605c5c54044ad15c1cd8ded7a17
yude <i@yude.jp>
2022-12-15 11:55:47 +0000

    fix: prefer `CF-Connecting-IP`

--- app/controllers/message.go
+++ app/controllers/message.go
@@ -10,16 +10,21 @@ import (
 
 func Post(c *fiber.Ctx) error {
 	value := c.FormValue("value")
+	client_ip := c.IP()
 
 	notice := "メッセージありがとうございました♪"
 
+	if c.GetRespHeader("CF-Connecting-IP") != "" {
+		client_ip = c.GetRespHeader("CF-Connecting-IP")
+	}
+
 	if value != "" {
-		database.AddMessage(value, c.IP()+":"+c.Port())
+		database.AddMessage(value, client_ip+":"+c.Port())
 	} else {
 		notice += " 送れているかは別として・・・"
 	}
 
-	log.Print("New message: " + value + " from " + c.IP() + ":" + c.Port())
+	log.Print("New message: " + value + " from " + client_ip + ":" + c.Port())
 
 	c.Set(fiber.HeaderContentType, fiber.MIMETextHTML)
 	return c.Render("static/index", fiber.Map{
