29ac9b2a95893a79aab2ea44bb7022fb23bdc359
yude <i@yude.jp>
2022-12-15 06:14:54 +0000

    feat: 性格

--- app/message.go
+++ app/message.go
@@ -10,12 +10,16 @@ import (
 func post_message(c *fiber.Ctx) error {
 	value := c.FormValue("value")
 
+	notice := "メッセージありがとうございました♪"
+
 	if value != "" {
 		database.AddMessage(value, c.IP()+":"+c.Port())
+	} else {
+		notice += " 送れているかは別として・・・"
 	}
 
 	c.Set(fiber.HeaderContentType, fiber.MIMETextHTML)
 	return c.Render("public/index", fiber.Map{
-		"notice": "メッセージありがとうございました♪",
+		"notice": notice,
 	})
 }
